c6987d6eee0a245435bf3d4308e2f3ca6a508c8e
Heroiu-Justinian <yhero0512@gmail.com>
2022-11-29 19:10:35 +0200

    first commit

---
+++ compiler.js
@@ -0,0 +1,17 @@
+const tokenizer = require('./tokenizer');
+const parser = require('./parser');
+const transformer = require('./transformer');
+const generateCode = require('./generateCode');
+
+
+module.exports = function compiler(input){
+  //1.Lexical analysis 
+  const tokens = tokenizer(input);
+  const lispAST = parser(tokens);
+  const jsAST = transformer(lispAST);
+  const jsCode = generateCode(jsAST);
+
+
+
+  return jsCode;
+}
